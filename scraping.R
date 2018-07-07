694440b166139d8388ad593c8ccee25577ae68ed
gnocco <gabriele.nocco@gmail.com>
2018-07-07 09:14:11 +0200

    initial

--- scraping.R
+++ scraping.R
@@ -6,3 +6,4 @@ URL <- "http://www.transfermarkt.com/premier-league/startseite/wettbewerb/GB1"
 WS <- read_html(URL)
 URLs <- WS %>% html_nodes(".hide-for-pad .vereinprofil_tooltip") %>% html_attr("href") %>% as.character()
 URLs <- paste0("http://www.transfermarkt.com",URLs)
+
